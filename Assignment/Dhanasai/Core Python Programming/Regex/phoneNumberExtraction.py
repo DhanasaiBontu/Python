9221615af70664de5ecff1209d1d12233fca6081
DhanasaiBontu <161912394+DhanasaiBontu@users.noreply.github.com>
2026-01-27 20:18:25 +0530

    Delete Assignment/Dhanasai/Core Python Programming/Regex/phoneNumberExtraction.py

--- Assignment/Dhanasai/Core Python Programming/Regex/phoneNumberExtraction.py
+++
@@ -1,6 +0,0 @@
-import re
-def extract_phone_number(text):
-    pattern = r'\b\d{10}\b'
-    return re.findall(pattern, text)
-text = "I changed my phon enumber. Now it is 9876543210"
-print(extract_phone_number(text))
